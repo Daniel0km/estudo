792ba550bc80e61dbeb2a5e6fe97472b22ddbd8c
Daniel0km <daniel.estudo2007@gmail.com>
2025-03-08 19:45:56 -0300

    Adicionei uma forma do cliente gerenciar o desconto

--- desconto.py
+++ desconto.py
@@ -1,19 +1,23 @@
-d1 = 0,10 - 0,5
-d2 = 0,1
-d3 = 0,15
-d4 = 0,20
+d1 = 1/20
+d2 = 1/10
+d3 = 15/100
+d4 = 1/5
 
 pp = float(input("Valor do produto: "))
 dp = str(input("O produto vai ter desconto? sim ou não "))
 if dp == "sim":
-    vd = float(input("Quanto vai ser o desconto? 5%, 10%, 15%, 20%: "))
+    vd = float(input("Quanto vai ser o desconto? 5%, 10%, 15%, 20%(0 para escolher o valor do desconto): "))
     if vd == 5:
-        print("o valor do produto de {} ira ser de {}".format(pp, pp-d1))
+        print("o valor do produto de {} ira ser de {}".format(pp, pp-(pp*d1)))
     elif vd == 10:
-        print("o valor do produto de {} ira ser de {}".format(pp, pp-d2))
+        print("o valor do produto de {} ira ser de {}".format(pp, pp-(pp*d2)))
     elif vd == 15:
         print("o valor do produto de {} ira ser de {}".format(pp, pp-(pp*d3)))
     elif vd == 20:
         print("o valor do produto de {} ira ser de {}".format(pp, pp-(pp*d4)))
+    elif vd == 0:
+        pvd = int(input("Qual o valor do desconto? "))
+        nd = pvd/100
+        print("o valor do produto de {} ira ser de {}".format(pp, pp-(pp*nd)))
 else:
     print("Tudo bem vai ser o valor cheio de {}".format(pp))
